91fc933ed765e3c6303b4d905d24c20c9bbc7ad0
Мишаня <mikhailok@gmail.com>
2015-02-20 16:39:02 +0200

    add main Module

---
+++ app/js/Tasks/tasksApp.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Мишаня on 13.02.2015.
+ */
+
+var tasksApp;
+tasksApp = angular.module("tasksApp", ["ui.router", "authModule", "signUpModule"]);
+
+tasksApp.constant( 'config', {
+    AUTO_INCREMENT_KEY: "autoIncrement",
+    ITEMS_DATA_KEY: "itemsData",
+    USERS_DATA_KEY: "usersData"
+});
+
+tasksApp.run(['$rootScope', '$state', function($rootScope, $state) {
+    $rootScope.$state = $state;
+}]);
+
+tasksApp.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise("/login");
+
+    $stateProvider
+        .state("Auth", {
+            url: "/login",
+            templateUrl: "app/templates/auth/login.html",
+            controller: "authController"
+        })
+        .state("Tasks", {
+            url: "/tasks",
+            templateUrl: "app/templates/tasks/tasks.html",
+            controller: "tasksController"
+        })
+        .state("SignUp", {
+            url: "/signup",
+            templateUrl: "app/templates/signUp/signup.html",
+            controller: "signUpController"
+        })
+}]);
